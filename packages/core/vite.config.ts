7ccbbe1bfb664604674abd15638db69629ba3b19
csca <csca.beats@gmail.com>
2024-01-04 12:25:49 +0100

    turbo setup

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/index.ts"),
+      name: "mixolydian",
+      formats: ["iife", "cjs", "umd", "es"],
+    },
+  },
+  plugins: [dts()],
+});
